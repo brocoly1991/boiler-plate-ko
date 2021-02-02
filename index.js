42b55ab3522d471fbad51ffa29a2b6fb5cfd73aa
brocoly91 <pignecro4@naver.com>
2021-02-03 08:33:54 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://pignecro4:wjdtjs@brocoly.wtzed.mongodb.net/<dbname>?retryWrites=true&w=majority', {
@@ -11,7 +18,19 @@ mongoose.connect('mongodb+srv://pignecro4:wjdtjs@brocoly.wtzed.mongodb.net/<dbna
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!!!!')
+    res.send('Hello World!!!! 새해복 많이')
+})
+
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success:true
+        })
+    })
 })
 
 app.listen(port, () => {
