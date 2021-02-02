073d9f9c956fe2ed5b91d350198a9bf3e9da5c02
brocoly91 <pignecro4@naver.com>
2021-02-03 08:42:52 +0900

    비밀 정보 보호2

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const port = 5000
 const bodyParser = require('body-parser');
 const { User } = require("./models/User");
 
+const config = require('./config/key');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://pignecro4:wjdtjs@brocoly.wtzed.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
     }).then(() => console.log('Successfully connected to mongodb'))
     .catch(e => console.error(e));
